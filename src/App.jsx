9644f72eb5816c7932d82725d3ddd55f9e0044bd
Koko-Dev <koko.webdev@gmail.com>
2024-09-01 13:17:06 -0400

    Menu.jsx:   Remove Menu code and bring it back to its basic component which only returns 'Menu'
    App.jsx:  Import createBrowserRouter and RouterProvider from react-router-dom; import Menu component as well from featurs/menu/Menu.jsx;  create const router which uses createBrowserRouter to create paths for the home page, menu page and cart page -- '/', '/menu', '/cart';  in App return statement, remove return code and instead call RouterProvider with prop router={ router }, i.e. the routes we previously defined
    
    // App.jsx
    import { createBrowserRouter, RouterProvider } from 'react-router-dom';
    import Home from './ui/Home.jsx';
    import Cart from './features/cart/Cart.jsx';
    import Menu from './features/menu/Menu.jsx';
    
    const router = createBrowserRouter( [
                                            {
                                                path: '/',
                                                element: <Home />,
                                            },
                                            {
                                                path: '/menu',
                                                element: <Menu />,
                                            },
                                            {
                                                path: '/cart',
                                                element: <Cart />,
                                            }
                                        ] );
    
    function App() {
        return (
            <RouterProvider router={ router } />
        );
    }
    
    export default App;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './ui/Home.jsx';
 import Cart from './features/cart/Cart.jsx';
+import Menu from './features/menu/Menu.jsx';
+
+
+const router = createBrowserRouter( [
+                                        {
+                                            path: '/',
+                                            element: <Home />,
+                                        },
+                                        {
+                                            path: '/menu',
+                                            element: <Menu />,
+                                        },
+                                        {
+                                            path: '/cart',
+                                            element: <Cart />,
+                                        }
+                                    ] );
 
 
 function App() {
     return (
-        <div>
-            <Home />
-            <Cart />
-        </div>
+        <RouterProvider router={ router } />
     );
 }
 
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,23 +1,7 @@
-import { useLoaderData } from 'react-router-dom';
-import { getMenu } from '../../services/apiRestaurant.js';
-import MenuItem from './MenuItem.jsx';
-
-
 function Menu() {
-    const menu = useLoaderData();
-    
     return (
-        <ul>
-            { menu.map( ( pizza ) => (
-                <MenuItem key={ pizza.id } pizza={ pizza } />
-            ) ) }
-        </ul>
+        <div>Menu</div>
     );
-}
-
-export async function loader() {
-    const menu = await getMenu();
-    return menu;
 }
 
 export default Menu;
