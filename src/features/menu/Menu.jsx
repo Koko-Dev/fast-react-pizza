d646a2443ff6f41e369f6a51ff1e557a66e07255
Koko-Dev <koko.webdev@gmail.com>
2024-09-05 16:58:01 -0400

    Menu.jsx:  Import named function useLoaderData() from react-router-dom, which is a hook the provides the value returned by our route loader; call useLoaderData() in const menu and log to the console where you will see the eighteen or so meals from the API
    // Menu.jsx
    import { getMenu } from '../../services/apiRestaurant.js';
    import { useLoaderData } from 'react-router-dom';
    
    function Menu() {
        const menu = useLoaderData();
        console.log(menu);
    
        return (
            <div>Menu</div>
        );
    }
    
    export async function loader() {
        const menu = await getMenu();
        return menu;
    }
    
    export default Menu;

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,7 +1,12 @@
 import { getMenu } from '../../services/apiRestaurant.js';
+import { useLoaderData } from 'react-router-dom';
 
 
 function Menu() {
+    const menu = useLoaderData();
+    console.log(menu);
+    
+    
     return (
         <div>Menu</div>
     );
